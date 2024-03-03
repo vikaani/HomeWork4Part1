ea6a94dcc4da48a82762e9e8da94c9028fd115eb
Viktoria <viktoriaani22@gmail.com>
2024-03-03 14:33:58 +0200

    Add testLabel

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        testLabel.text = "Homework 4, part 1"
     }
 
 
