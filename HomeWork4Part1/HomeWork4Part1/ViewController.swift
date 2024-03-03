5a038ab70baa32c995ff64b45f48dc053ea80715
Viktoria <viktoriaani22@gmail.com>
2024-03-03 17:09:35 +0200

    Complete HomeWork4Part1 tasks

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var testLabel: UILabel!
+    @IBOutlet var clickHereButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         testLabel.text = "Homework 4, part 1"
     }
-
-
+    
+    @IBAction func didTapClichHereButton( _ sender: UIButton) {
+        let currentTestLabel = testLabel.text
+        let text1 = "Homework 4, part 1"
+        let text2 = "Hello"
+        testLabel.text = (currentTestLabel == text1) ? text2 : text1
+    }
 }
 
